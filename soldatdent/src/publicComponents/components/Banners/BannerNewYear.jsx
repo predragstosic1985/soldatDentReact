d0fa982535c8e2587be5f59aaaad7678a39ae42b
predragstosic1985 <predragstosic1985@gmail.com>
2023-01-05 14:01:12 +0100

    Snow fall in app setup

--- soldatdent/src/publicComponents/components/Banners/BannerNewYear.jsx
+++ soldatdent/src/publicComponents/components/Banners/BannerNewYear.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Button, Modal } from "semantic-ui-react";
 import Animate from "react-smooth";
 import styles from "./style/BannerNewYear.module.css";
+import Snowfall from "react-snowfall";
 
 const BannerNewYear = ({ setOpenBanner, openBanner }) => {
   return (
@@ -10,11 +11,31 @@ const BannerNewYear = ({ setOpenBanner, openBanner }) => {
         onClose={() => setOpenBanner(false)}
         onOpen={() => setOpenBanner(true)}
         open={openBanner}
-        size="tiny"
         className={styles.bannerStyle}
       >
-        <Modal.Content className={styles.heroImage}>
-          <Modal.Description className={styles.heroImage}>
+        <Modal.Content>
+          <Snowfall
+            // color="red"
+            // Applied to the canvas element
+            // style={{ background: "#fff" }}
+            // Controls the number of snowflakes that are created (default 150)
+            style={{
+              backgroundColor: "#ba8e40",
+              backgroundImage: "linear-gradient(to right, #00b4db, #0083b0)",
+              // backgroundImage:
+              //   "linear-gradient( 109.6deg, rgba(156,252,248,1) 11.2%, rgba(110,123,251,1) 91.1% )",
+              height: "100%",
+              width: "100%",
+              // backgroundPosition: "center",
+              // backgroundRepeat: "no-repeat",
+              // backgroundSize: "cover",
+              // position: "relative",
+              // backgroundImage:
+              //   "url('../../../../Assets/Images/newYearBanner.jpg')",
+            }}
+            snowflakeCount={1000}
+          />
+          <Modal.Description>
             <div>
               <Animate
                 from={{ opacity: 0.1 }}
@@ -23,22 +44,26 @@ const BannerNewYear = ({ setOpenBanner, openBanner }) => {
                 easing="ease-in"
               >
                 <div className={styles.heroText}>
-                  <h1>Novogodisnji popusti su stigli ranije!</h1>
-
+                  {/* <h1>Novogodisnji popusti su stigli ranije!</h1> */}
                   <p className={styles.headerTextHead}>
-                    Specijalni popusti na:
+                    Srećne praznike želi vam naša ordinacija!
                   </p>
-                  <p className={styles.headerTextFirst}>izbeljivanje</p>
-                  <p className={styles.headerTextSecond}>
+                  {/* <p className={styles.headerTextFirst}>izbeljivanje</p> */}
+                  {/* <p className={styles.headerTextSecond}>
                     implante bezmetalne krunice i{" "}
+                  </p> */}
+                  {/* <p className={styles.headerTextThird}>botoks tretmane! </p> */}
+                  <p className={styles.headerTextThird}>
+                    <br />
+                    Posetite nas čekaju vas pokloni{" "}
                   </p>
-                  <p className={styles.headerTextThird}>botoks tretmane! </p>
 
+                  {/* 
                   <Button
                     circular
                     icon="check"
                     onClick={() => setOpenBanner(false)}
-                  />
+                  /> */}
                 </div>
               </Animate>
             </div>
